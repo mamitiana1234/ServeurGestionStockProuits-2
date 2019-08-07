46047b6f5bacbedcb2ef8255596e05b9a6fe91ef
mamitiana1234 <mamitianatovonirina2@gmail.com>
2019-08-07 13:12:57 +0200

    first commit

---
+++ src/main/java/com/example/demo/ServeurGestionStockProuits1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProuits1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProuits1Application.class, args);
+	}
+
+}
